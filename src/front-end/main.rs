e9f811f162e5415a431c678a6b28e5fdbd05b989
Tim Zhang <cyzhang0339@gmail.com>
2023-05-22 00:02:02 -0700

    Testing git

--- src/front-end/main.rs
+++ src/front-end/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+
+}
+
+fn minimax(turn: &bool, alpha: &i32, beta: &i32, board: &u64, depth: &u32) {
+    if turn {
+
+    }
+}
+
+fn gameover() -> bool {
+
+}
+
+fn place(board: &u64) -> u64{
+    
 }
